cee2c9ec28b4818322f848cfe9005e4bef48e023
Harish <vharish836@gmail.com>
2018-04-30 11:42:59 +0530

    Fix dep issues due to incorrect imports and fork

--- vendor/github.com/gorilla/rpc/json/client.go
+++ vendor/github.com/gorilla/rpc/json/client.go
@@ -22,7 +22,7 @@ type clientRequest struct {
 	// A String containing the name of the method to be invoked.
 	Method string `json:"method"`
 	// Object to pass as request parameter to the method.
-	Params [1]interface{} `json:"params"`
+	Params []interface{} `json:"params"`
 	// The request id. This can be of any type. It is used to match the
 	// response with the request that it is replying to.
 	Id uint64 `json:"id"`
@@ -36,12 +36,22 @@ type clientResponse struct {
 }
 
 // EncodeClientRequest encodes parameters for a JSON-RPC client request.
-func EncodeClientRequest(method string, args interface{}) ([]byte, error) {
-	c := &clientRequest{
-		Method: method,
-		Params: [1]interface{}{args},
-		Id:     uint64(rand.Int63()),
+func EncodeClientRequest(method string, args []interface{}) ([]byte, error) {
+	var c *clientRequest
+	if args != nil {
+		c = &clientRequest{
+			Method: method,
+			Params: args,
+			Id:     uint64(rand.Int63()),
+		}
+	} else {
+		c = &clientRequest{
+			Method: method,
+			Params: make([]interface{}, 0),
+			Id:     uint64(rand.Int63()),
+		}
 	}
+
 	return json.Marshal(c)
 }
 
